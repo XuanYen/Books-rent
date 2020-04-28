b1b54ccadb64bba9357dd5118744bc8e806c28a1
XuanYen <xuanyenbentre@gmail.com>
2020-04-28 23:45:03 +0700

    bai 11 Static file

---
+++ ExpressJS/bai11-static-file/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const bodyParser = require('body-parser');
+var userRoute=require("./routes/user.route")
+const port=3000;
+const app=express();
+app.set('view engine', 'pug');
+app.set('views','./views'); 
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))
+app.get('/',(req, res)=>res.render('index',{
+    name: 'AAA'
+}));
+
+app.use('/users', userRoute);
+
+
+app.listen(port,()=>console.log('server listening on port'+port));
