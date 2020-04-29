32cb9377d0a26063bc4ed62d5e19076bc01b6fce
XuanYen <xuanyenbentre@gmail.com>
2020-04-29 09:18:34 +0700

    bai5 nodemon and bai6 lowdb

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -3,22 +3,27 @@ const app=express();
 const bodyParser = require('body-parser');
 const port=3000;
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+//Tao object bang cach goi low tra ve adapter
+//Dung db tao truy xuat du lieu
+db.defaults({ users: []})
+  .write()
+//Set cac gia tri default  
 app.set('view engine', 'pug');
 app.set('views','./views'); 
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-//Tao id cho body roi luu vao array nay.
-var users=[
-    {id: 1, name:'Thanh'},
-    {id: 2, name:'Phong'}
-];
+
 app.get('/',(req, res)=>res.render('index',{
     name: 'AAA'
 }));
 
 app.get('/users',(req,res)=>res.render('users/index',{
-    users: users
+    users: db.get('users').value()
 }));
 app.get('/users/search', (req,res)=>{
     var q=req.query.q;
@@ -31,10 +36,9 @@ app.get('/users/create',(req,res)=>res.render('users/create'));
 //Tao endpoint trả lời được khi nhận request
 //Chua validate, nhap gi cung gui duoc
 app.post('/users/create',(req,res)=>{
-    console.log(req.body)
-    users.push(req.body);
+    db.get('users').push(req.body).write(); //de ghi lai trong file
     //Sau khi luu thong tin, chuyen ng dung ve lai trang user
     //method của res. 
-    res.redirect('back');
+    res.redirect('/users');
 });
 app.listen(port,()=>console.log('server listening on port'+port));
