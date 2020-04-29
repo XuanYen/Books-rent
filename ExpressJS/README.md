4989bb120a7507388950495e7db6f2eed9416003
XuanYen <xuanyenbentre@gmail.com>
2020-04-29 09:44:51 +0700

    bai8-express-router

--- ExpressJS/README.md
+++ ExpressJS/README.md
@@ -4,3 +4,7 @@
 ## Bai2. Template engine
 ## Bai3. Query parameters
 ## Bai4. Post method
+## Bai5. Nodemon
+## Bai6. Tich hop database (lowdb)
+## Bai7. Route param
+## Bai8. Express router
--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,17 +1,9 @@
 const express=require('express');
 const app=express();
+var userRoute=require("./routes/user.route")
 const bodyParser = require('body-parser');
 const port=3000;
-var shortid=require('shortid');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-//Tao object bang cach goi low tra ve adapter
-//Dung db tao truy xuat du lieu
-db.defaults({ users: []})
-  .write()
-//Set cac gia tri default  
+
 app.set('view engine', 'pug');
 app.set('views','./views'); 
 
@@ -22,38 +14,5 @@ app.get('/',(req, res)=>res.render('index',{
     name: 'AAA'
 }));
 
-app.get('/users',(req,res)=>res.render('users/index',{
-    users: db.get('users').value()
-}));
-app.get('/users/search', (req,res)=>{
-    var q=req.query.q;
-    var matchedUsers=users.filter(user=>user.name.toLowerCase().indexOf(q.toLowerCase())!== -1);
-    res.render('users/index',{
-        users: matchedUsers
-    })
-})
-app.get('/users/create',(req,res)=>res.render('users/create'));
-//Tao endpoint trả lời được khi nhận request
-//Chua validate, nhap gi cung gui duoc
-
-app.get('/users/:id',(req,res)=>{
-    //route param chứ không phải query
-    //Khi get request gửi tới url đó thì sẽ chạy callback, cái gì đó sẽ được lưu ở cái biến nằm trong req.params.id
-    var id= req.params.id; //string-> chuyen sang number
-    //Lay user trong db ra
-    var user=db.get('users').find({ id: id }).value()
-    //Tim id trung voi id, tim ra 1 cai ele dau tien co value cua key la nhan duoc tu param
-    res.render('users/view',{
-        user: user
-    }); //Phai define view.pug
-});
-
-
-app.post('/users/create',(req,res)=>{
-    req.body.id=shortid.generate();
-    db.get('users').push(req.body).write(); //de ghi lai trong file
-    //Sau khi luu thong tin, chuyen ng dung ve lai trang user
-    //method của res. 
-    res.redirect('/users');
-});
+app.use('/users', userRoute);
 app.listen(port,()=>console.log('server listening on port'+port));
