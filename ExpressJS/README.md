a1f820001b64a79b6db0c11546dbd392c2d67433
XuanYen <xuanyenbentre@gmail.com>
2020-04-29 09:08:41 +0700

    bai4-post-method

--- ExpressJS/README.md
+++ ExpressJS/README.md
@@ -2,3 +2,5 @@
 
 ## Bai1. CodersTokyo
 ## Bai2. Template engine
+## Bai3. Query parameters
+## Bai4. Post method
--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const app=express();
+const bodyParser = require('body-parser');
 const port=3000;
 
 app.set('view engine', 'pug');
 app.set('views','./views'); 
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+//Tao id cho body roi luu vao array nay.
 var users=[
     {id: 1, name:'Thanh'},
     {id: 2, name:'Phong'}
@@ -18,11 +22,19 @@ app.get('/users',(req,res)=>res.render('users/index',{
 }));
 app.get('/users/search', (req,res)=>{
     var q=req.query.q;
-    //Filter user theo tu khoa nay de xem ten co match voi q nay khong
     var matchedUsers=users.filter(user=>user.name.toLowerCase().indexOf(q.toLowerCase())!== -1);
-    //Chuyen het ve chu thuong de loc duoc moi truong hop
     res.render('users/index',{
         users: matchedUsers
     })
 })
+app.get('/users/create',(req,res)=>res.render('users/create'));
+//Tao endpoint trả lời được khi nhận request
+//Chua validate, nhap gi cung gui duoc
+app.post('/users/create',(req,res)=>{
+    console.log(req.body)
+    users.push(req.body);
+    //Sau khi luu thong tin, chuyen ng dung ve lai trang user
+    //method của res. 
+    res.redirect('back');
+});
 app.listen(port,()=>console.log('server listening on port'+port));
