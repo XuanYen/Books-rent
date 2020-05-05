022ae7b7ac1c9083fcaf1e45f03fb611529c4ffa
XuanYen <xuanyenbentre@gmail.com>
2020-05-05 23:38:19 +0700

    upload avatar

--- ExpressJS/routes/user.route.js
+++ ExpressJS/routes/user.route.js
@@ -1,8 +1,12 @@
 var express = require('express');
-var router = express.Router()
+var multer  = require('multer');
+
 var validate=require('../validate/user.validate');
 var controller=require('../controllers/user.controller')
 var authMiddleware=require('../middleware/auth.middleware');
+var upload = multer({ dest: './public/uploads/' });
+//upload vao dau?
+var router = express.Router()
 
 router.get('/',authMiddleware.requireAuth,controller.index);
 
@@ -17,7 +21,10 @@ router.get('/create',controller.create);
 
 router.get('/:id',controller.get);
 
-router.post('/create',validate.postCreate,controller.postCreate);
-
-
+router.post('/create',
+upload.single('avatar'),
+validate.postCreate,
+ controller.postCreate);
+//single ý là 1 file đơn lẻ, check field avatar gui client len
+//req.file chứa file mình upload lên 
 module.exports = router
